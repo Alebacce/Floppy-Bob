c180a4e1d0c4601b971f3b5895bc074565c96f19
Alessandro Baccelli <alessandro.baccelli17@gmail.com>
2025-06-15 00:29:57 +0200

    Fix, finally, the invsible pipe bug

--- Assets/PipeSpawnerScript.cs
+++ Assets/PipeSpawnerScript.cs
@@ -1,8 +1,11 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PipeSpawnerScript : MonoBehaviour
 {
     public GameObject pipe;
+    public float spawnRate = 2;
+    private float timer = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +15,14 @@ public class PipeSpawnerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Instantiate(pipe, transform.position, transform.rotation);
+        if (timer < spawnRate)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            Instantiate(pipe, transform.position, transform.rotation);
+            timer = 0;
+        }
     }
 }
