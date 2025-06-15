e798985d3e43bc0e5c72423fc41ff2d9d65d6090
Alessandro Baccelli <alessandro.baccelli17@gmail.com>
2025-06-15 12:48:51 +0200

    Game Over && replay logic

--- Assets/birdscript.cs
+++ Assets/birdscript.cs
@@ -4,18 +4,28 @@ public class birdscript : MonoBehaviour
 {
     public Rigidbody2D myRigidBody;
     public float flapStrenght;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        gameObject.name = "Bob Uccellacci";
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (birdIsAlive && Input.GetKeyDown(KeyCode.Space))
         {
             myRigidBody.linearVelocity = Vector2.up * flapStrenght;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        birdIsAlive = false;
+        Debug.Log(birdIsAlive);
+        logic.gameOver();
     }
 }
